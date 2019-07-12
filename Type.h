e43da7a172fa6d5f10819f1fea70266cd30c02f5
nicehero <85948597@qq.com>
2019-07-12 18:43:16 +0800

    SafeInteger

--- Type.h
+++ Type.h
@@ -5,6 +5,8 @@
 #include <string>
 #include <memory>
 #include <memory.h>
+#include <utility>
+
 const int NETWORK_BUF_SIZE = 1024 * 16;
 
 using ui8 = uint8_t;
@@ -22,8 +24,113 @@ const ui16 PROTOCOL_VERSION_AVAILABLE_NTF = 0xFFFD;// 65533
 const ui16 KCP_READY_NTF = 0xFFF0;// 65529
 const ui16 KCP_CLOSE = 0xFFEF;// 65528
 
+
 namespace nicehero 
 {
+	using oper_uint_base = ui32;
+	using store_uint_base = ui64;
+	const store_uint_base oper_uint_base_max = store_uint_base(UINT32_MAX);
+
+	class OperUInt;
+	class StoreUInt
+	{
+		friend class OperUInt;
+	public:
+		StoreUInt() {
+			impl = store_uint_base(0);
+		}
+		StoreUInt(store_uint_base impl_) {
+			impl = store_uint_base(impl_);
+		}
+		bool operator>=(const store_uint_base& other) const {
+			return impl >= other;
+		}
+		bool operator<=(const store_uint_base& other) const {
+			return impl <= other;
+		}
+		bool operator>(const store_uint_base& other) const {
+			return impl > other;
+		}
+		bool operator<(const store_uint_base& other) const {
+			return impl < other;
+		}
+		bool operator==(const store_uint_base& other) const {
+			return impl == other;
+		}
+		bool operator!=(const store_uint_base& other) const {
+			return impl != other;
+		}
+		operator store_uint_base() const {
+			return impl;
+		}
+		bool add(const store_uint_base& other) {
+			store_uint_base ret = impl + other;
+			if (ret < impl || ret < other) {
+				return false;
+			}
+			impl = ret;
+			return true;
+		}
+		bool add(oper_uint_base other) {
+			store_uint_base o(other);
+			return add(o);
+		}
+		bool add(OperUInt other);
+		bool minus(const store_uint_base& other) {
+			if (other > impl) {
+				return false;
+			}
+			impl -= other;
+			return true;
+		}
+		bool minus(oper_uint_base other) {
+			store_uint_base o(other);
+			return minus(o);
+		}
+		bool minus(OperUInt other);
+		std::pair<bool, OperUInt> toOper();
+		store_uint_base impl;
+	};
+
+	class OperUInt
+	{
+		friend class StoreUInt;
+	public:
+		OperUInt() {
+			impl = oper_uint_base(0);
+		}
+		OperUInt(oper_uint_base impl_) {
+			impl = impl_;
+		}
+		StoreUInt operator+(const OperUInt& right)const {
+			return StoreUInt(store_uint_base(impl) + store_uint_base(right.impl));
+		}
+		StoreUInt operator*(const OperUInt& right)const {
+			return StoreUInt(store_uint_base(impl) * (store_uint_base(right.impl)));
+		}
+		StoreUInt operator/(const OperUInt& right)const {
+			return StoreUInt(store_uint_base(impl) / store_uint_base(right.impl));
+		}
+		StoreUInt operator%(const OperUInt& right)const {
+			return StoreUInt(store_uint_base(impl) % (store_uint_base(right.impl)));
+		}
+		oper_uint_base impl;
+	};
+	inline bool StoreUInt::add(OperUInt other) {
+		return add(other.impl);
+	}
+	inline bool StoreUInt::minus(OperUInt other) {
+		return minus(other.impl);
+	}
+	inline std::pair<bool, OperUInt> StoreUInt::toOper()
+	{
+		if (impl > oper_uint_base_max)
+		{
+			return std::make_pair(false, OperUInt(oper_uint_base(0)));
+		}
+		return std::make_pair(true, OperUInt(oper_uint_base(impl)));
+	}
+
 	struct Binary
 	{
 		Binary() {}
@@ -41,6 +148,174 @@ namespace nicehero
 	};
 
 }
+
+template <typename T>
+class Initializable
+{
+public:
+	Initializable()
+	{
+		impl = 0;
+	}
+	Initializable(const T& t)
+	{
+		impl = t;
+	}
+	Initializable<T> operator+(const Initializable<T>& right) const
+	{
+		return Initializable<T>(impl + right.impl);
+	}
+	Initializable<T> operator-(const Initializable<T>& right) const
+	{
+		return Initializable<T>(impl - right.impl);
+	}
+	Initializable<T> operator*(const Initializable<T>& right) const
+	{
+		return Initializable<T>(impl * right.impl);
+	}
+	Initializable<T> operator/(const Initializable<T>& right) const
+	{
+		return Initializable<T>(impl / right.impl);
+	}
+	Initializable<T> operator%(const Initializable<T>& right) const
+	{
+		return Initializable<T>(impl % right.impl);
+	}
+	Initializable<T> operator>>(const Initializable<T>& right) const
+	{
+		return Initializable<T>(impl >> right.impl);
+	}
+	Initializable<T> operator<<(const Initializable<T>& right) const
+	{
+		return Initializable<T>(impl << right.impl);
+	}
+	Initializable<T> operator&(const Initializable<T>& right) const
+	{
+		return Initializable<T>(impl & right.impl);
+	}
+	Initializable<T> operator|(const Initializable<T>& right) const
+	{
+		return Initializable<T>(impl | right.impl);
+	}
+	Initializable<T> operator^(const Initializable<T>& right) const
+	{
+		return Initializable<T>(impl ^ right.impl);
+	}
+	Initializable<T>& operator=(const Initializable<T>& other)
+	{
+		impl = other.impl;
+		return *this;
+	}
+	Initializable<T>& operator+=(const Initializable<T>& other)
+	{
+		impl += other.impl;
+		return *this;
+	}
+	Initializable<T>& operator-=(const Initializable<T>& other)
+	{
+		impl -= other.impl;
+		return *this;
+	}
+	Initializable<T>& operator*=(const Initializable<T>& other)
+	{
+		impl *= other.impl;
+		return *this;
+	}
+	Initializable<T>& operator/=(const Initializable<T>& other)
+	{
+		impl /= other.impl;
+		return *this;
+	}
+	Initializable<T>& operator%=(const Initializable<T>& other)
+	{
+		impl %= other.impl;
+		return *this;
+	}
+	Initializable<T>& operator>>=(const Initializable<T>& other)
+	{
+		impl >>= other.impl;
+		return *this;
+	}
+	Initializable<T>& operator<<=(const Initializable<T>& other)
+	{
+		impl <<= other.impl;
+		return *this;
+	}
+	Initializable<T>& operator&=(const Initializable<T>& other)
+	{
+		impl &= other.impl;
+		return *this;
+	}
+	Initializable<T>& operator|=(const Initializable<T>& other)
+	{
+		impl |= other.impl;
+		return *this;
+	}
+	Initializable<T>& operator^=(const Initializable<T>& other)
+	{
+		impl ^= other.impl;
+		return *this;
+	}
+	Initializable<T>& operator ++()
+	{
+		++impl;
+		return *this;
+	}
+	const Initializable<T> operator ++(int)
+	{
+		Initializable<T> old = *this;
+		++impl;
+		return old;
+	}
+	Initializable<T>& operator --()
+	{
+		--impl;
+		return *this;
+	}
+	const Initializable<T> operator --(int)
+	{
+		Initializable<T> old = *this;
+		--impl;
+		return old;
+	}
+	Initializable<T> operator~()
+	{
+		return ~impl;
+	}
+	bool operator!()
+	{
+		return !impl;
+	}
+	bool operator==(const Initializable<T>& other)
+	{
+		return impl == other.impl;
+	}
+	bool operator!=(const Initializable<T>& other)
+	{
+		return impl != other.impl;
+	}
+	bool operator<(const Initializable<T>& other)
+	{
+		return impl < other.impl;
+	}
+	bool operator>(const Initializable<T>& other)
+	{
+		return impl > other.impl;
+	}
+	bool operator>=(const Initializable<T>& other)
+	{
+		return impl >= other.impl;
+	}
+	bool operator<=(const Initializable<T>& other)
+	{
+		return impl <= other.impl;
+	}
+	operator T()
+	{
+		return impl;
+	}
+	T impl;
+};
 
 
 #endif
